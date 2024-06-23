03cd1a735f1730d80be7dea5d4737163d4c88887
DanilaProkopenko <danilaprok20@gmail.com>
2024-06-23 18:43:43 +0300

    add adaptive to postcard and welcome screen

--- components/post-card/post-card__big/post-card__big.php
+++ components/post-card/post-card__big/post-card__big.php
@@ -13,7 +13,7 @@ if ($args) {
 }
 
 ?>
-<div class="post-card__big main-padding">
+<div class="post-card__big">
     <div class="post-card__big__content__wrapper">
         <div class="post-card__big__content">
             <div class="post-card__big__title">
@@ -31,54 +31,65 @@ if ($args) {
                     Дизайнер в широком смысле этого слова Работаю и с сайтами, и с различной полиграфией, и также со всяким промо–продуктом, по типу: баннеры, стенды, вывески. -->
                 </div>
             </div>
-            <div class="post-card__big__meta">
-                <div class="post-card__big__meta__item">
-                    <div class="post-card__big__meta__item__variable">Год</div>
-                    <div class="post-card__big__meta__item__value">2024</div>
+            <?
+            $post_meta_repeater = get_field('post_meta-repeater', $id);
+            if (have_rows('post_meta-repeater', $id)) :
+            ?>
+                <div class="post-card__big__meta">
+                    <?
+                    while (have_rows('post_meta-repeater', $id)) : the_row();
+                        $post_meta_variable = get_sub_field('post_meta-variable');
+                        $post_meta_value = get_sub_field('post_meta-value');
+                    ?>
+                        <div class="post-card__big__meta__item">
+                            <div class="post-card__big__meta__item__variable"><?= $post_meta_variable ?></div>
+                            <div class="post-card__big__meta__item__value"><?= $post_meta_value ?></div>
+                        </div>
+                    <? endwhile; ?>
                 </div>
-                <div class="post-card__big__meta__item">
-                    <div class="post-card__big__meta__item__variable">Роль</div>
-                    <div class="post-card__big__meta__item__value">Дизайнер, разработчик</div>
-                </div>
-                <div class="post-card__big__meta__item">
-                    <div class="post-card__big__meta__item__variable">Делал</div>
-                    <div class="post-card__big__meta__item__value">Концепт, UI/UX дизайн, прототип, разработка</div>
-                </div>
-                <div class="post-card__big__meta__item">
-                    <div class="post-card__big__meta__item__variable">Сайт</div>
-                    <div class="post-card__big__meta__item__value">nika-yug.ru</div>
-                </div>
-            </div>
+            <? endif ?>
         </div>
-        <div class="post-card__meta__team">
-            <div class="post-card__meta__team__images__wrapper">
-                <img src="" alt="" class="post-card__meta__teaam__imge__source">
-                <img src="" alt="" class="post-card__meta__team__image__source">
+        <?
+        $users = get_field("post_meta_team", $id);
+        if ($users) :
+        ?>
+            <div class="post-card__meta__team">
+                <div class="post-card__meta__team__images__wrapper">
+                    <img src="<?= esc_attr(get_avatar_url('danilaprok20@gmail.com')); ?>" alt="Аватар пользователя" class="post-card__meta__teaam__imge__source">
+                    <? foreach ($users as $user) : ?>
+                        <img src="<?= esc_attr(get_avatar_url($user['ID'])); ?>" alt="Аватар пользователя" class="post-card__meta__teaam__imge__source">
+                    <? endforeach; ?>
+                </div>
+                <div class="post-card__meta__team__name">
+                    Совместно с
+                    <? foreach ($users as $user) : ?>
+                        <a href="<?= esc_attr($user['user_url']) ?>">
+                            <?= esc_attr($user['nickname']) ?>
+                        </a>
+                    <? endforeach; ?>
+                </div>
             </div>
-            <div class="post-card__meta__team__name">
-                Совместно с <a href="https://www.zerna.design">zerna.design</a>
-            </div>
-        </div>
+        <? endif; ?>
     </div>
     <div class="post-card__big__gallery">
-        <div class="f-carousel">
-            <div class="f-carousel__slide">
+        <div class="f-carousel" data-gallery="<?= $id ?>">
+            <a href="<?= esc_url($thumb_img_medium_large); ?> " data-fancybox="<?= $id ?>" class="f-carousel__slide">
                 <picture>
                     <source media="(max-width: 768px)" srcset="<?= esc_url($thumb_img_medium_large); ?>" />
                     <source media="(min-width: 769px)" srcset="<?= esc_url($thumb_img_medium_large); ?>" />
-                    <img class="post-card__big__gallery__img__source" src="<?= esc_url($thumb_img_medium_large); ?>" alt="Обложка записи <?= $title ?>" loading="lazy">
+                    <img class="post-card__big__gallery__img__source _cover" src="<?= esc_url($thumb_img_medium_large); ?>" alt="Обложка записи <?= $title ?>" loading="lazy">
                 </picture>
-            </div>
+            </a>
             <?php
             if ($gallery) :
                 foreach ($gallery as $image) : ?>
-                    <div class="f-carousel__slide">
+                    <a href="<?= esc_url($image['sizes']['2048x2048']); ?>" data-fancybox="<?= $id ?>" class=" f-carousel__slide">
                         <picture>
                             <source media="(max-width: 768px)" srcset="<?= esc_url($image['sizes']['1536x1536']); ?>" />
                             <source media="(min-width: 769px)" srcset="<?= esc_url($image['sizes']['2048x2048']); ?>" />
                             <img class="post-card__big__gallery__img__source" src="<?php echo esc_url($image['sizes']['2048x2048']); ?>" alt="<?php echo esc_attr($image['alt']) ?>" loading="lazy">
                         </picture>
-                    </div>
+                    </a>
             <?php endforeach;
             endif;
             ?>
