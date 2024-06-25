23154b041c4fd0b20ae09cb61f2884058306befb
DanilaProkopenko <danilaprok20@gmail.com>
2024-06-25 18:46:13 +0300

    new layout with description card on post card big

--- components/header/header.php
+++ components/header/header.php
@@ -4,58 +4,54 @@
 
 <header class="header__wrapper">
     <div class="header">
-        <a href="/" class="header__logo">
-            <h1 class="header__logo__title">
-                prokopenko
-            </h1>
-        </a>
-        <a href="/" class="header__logo">
-            <img src="<?= esc_attr(get_avatar_url('danilaprok20@gmail.com')); ?>" alt="Аватар пользователя" class="header__logo__source">
-
-            <h1 class="header__logo__title">
-                Данила Прокопенко
-                <span class="header__logo__caption">
-                    Графический и веб–дизайнер
-                </span>
-            </h1>
-        </a>
-        <!-- <nav class="header__links">
-            <ul class="header__links__list">
-                <li class="header__link__item"><a href="" class="header__link">работы</a></li>
-                <li class="header__link__item"><a href="" class="header__link">обо мне</a></li>
-                <li class="header__link__item"><a href="" class="header__link">резюме</a></li>
-            </ul>
-        </nav>
-        <nav class="header__links header__links_social">
-            <ul class="header__links__list">
-                <li class="header__link__item"><a href="" class="header__link">telegram</a></li>
-                <li class="header__link__item"><a href="" class="header__link">instagram</a></li>
-                <li class="header__link__item"><a href="" class="header__link">email</a></li>
-            </ul>
-        </nav> -->
-        <div class="header__description">
-            <p>
-                Дизайнер в широком смысле этого слова. 
-                Работаю и с сайтами, и с различной полиграфией, и также с различным промо–продуктом по типу: баннеры, стенды, вывести и тд.
-            </p>
-            <p>
-                Коммерческий опыт — 4 года. Сейчас работаю в <a href="https://www.zerna.design/" target="_blank">zerna.design.</a> Живу в Ростове–на–Дону.
-            </p>
-            <p>
-                Мне невероятно повезло работать c потрясающими людьми, которые помогают, учат и терпят многочисленные вопросы. Спасибо вам)
-            </p>
+        <div class="header__top">
+            <a href="/" class="header__top__logo">
+                <h1>
+                    prokopenko
+                </h1>
+            </a>
+            <div class="header__top__burger">
+                <svg width="21" height="17" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 2H21M0 8.27586H21M0 15H21" stroke="white" stroke-width="2.5" />
+                </svg>
+            </div>
         </div>
-        <nav class="header__links">
-            <ul class="header__links__list">
-                <li class="header__link__item"><a href="" class="header__link">danilaprok20@gmail.com</a></li>
-                <li class="header__link__item"><a href="" class="header__link">Резюме</a></li>
+        <div class="header__description">
+            <div class="header__description__avatar">
+                <img src="<?= esc_attr(get_avatar_url('danilaprok20@gmail.com')); ?>" alt="" class="header__description__avatar__source">
+                <div class="header__description__avatar__title">
+                    <div class="header__description__avatar__title_name">
+                        Данила Прокопенко
+                    </div>
+                    <div class="header__description__avatar__title_caption">
+                        Графический и веб–дизайнер
+                    </div>
+                </div>
+            </div>
+            <div class="header__description__content">
+                <p>
+                    Дизайнер в широком смысле этого слова.
+                    Работаю и с сайтами, и с различной полиграфией, и также с различным промо–продуктом по типу: баннеры, стенды, вывести и тд.
+                </p>
+                <p>
+                    Коммерческий опыт — 4 года. Сейчас работаю в <a href="https://www.zerna.design/" target="_blank">zerna.design.</a> Живу в Ростове–на–Дону.
+                </p>
+                <p>
+                    Мне невероятно повезло работать c потрясающими людьми, которые помогают, учат и терпят многочисленные вопросы. Спасибо вам)
+                </p>
+            </div>
+        </div>
+        <div class="header__bottom">
+            <ul class="header__bottom__links">
+                <li class="header__bottom__links__item"><a href="" class="header__bottom__links__item__link">danilaprok20@gmail.com</a></li>
+                <li class="header__bottom__links__item"><a href="" class="header__bottom__links__item__link">Резюме</a></li>
+                <li class="header__bottom__links__item"><a href="" class="header__bottom__links__item__link">Telegram</a></li>
+                <li class="header__bottom__links__item"><a href="" class="header__bottom__links__item__link">Instagram</a></li>
             </ul>
-            <ul class="header__links__list">
-                <li class="header__link__item"><a href="" class="header__link">Telegram</a></li>
-                <li class="header__link__item"><a href="" class="header__link">Instagram</a></li>
-            </ul>
-        </nav>
-
-
+            <div class="header__bottom__year">
+                <div class="header__bottom__year__update">Last update: 13.07.2024</div>
+                <div class="header__bottom__year__title">© 2024 Made by me</div>
+            </div>
+        </div>
     </div>
 </header>
--- components/post-card/post-card__big/post-card__big.php
+++ components/post-card/post-card__big/post-card__big.php
@@ -15,64 +15,6 @@ if ($args) {
 
 ?>
 <div class="post-card__big">
-    <div class="post-card__big__content__wrapper">
-        <div class="post-card__big__content">
-            <div class="post-card__big__title">
-                <div class="post-card__big__heading">
-                    <?= $title ?>
-                </div>
-                <div class="post-card__big__short-description">
-                    <?= $short_description ?>
-                    <?
-                    // echo '<pre>';
-                    // var_dump($gallery);
-                    // echo '</pre>';
-                    ?>
-                    <!-- Сделал в команде в студии zerna.design новый сайт для производителя водосточных систем. Упор был направлен на продвижения покрытий.
-                    Дизайнер в широком смысле этого слова Работаю и с сайтами, и с различной полиграфией, и также со всяким промо–продуктом, по типу: баннеры, стенды, вывески. -->
-                </div>
-            </div>
-            <?
-            $post_meta_repeater = get_field('post_meta-repeater', $id);
-            if (have_rows('post_meta-repeater', $id)) :
-            ?>
-                <div class="post-card__big__meta">
-                    <?
-                    while (have_rows('post_meta-repeater', $id)) : the_row();
-                        $post_meta_variable = get_sub_field('post_meta-variable');
-                        $post_meta_value = get_sub_field('post_meta-value');
-                    ?>
-                        <div class="post-card__big__meta__item">
-                            <div class="post-card__big__meta__item__variable"><?= $post_meta_variable ?></div>
-                            <div class="post-card__big__meta__item__value"><?= $post_meta_value ?></div>
-                        </div>
-                    <? endwhile; ?>
-                </div>
-            <? endif ?>
-        </div>
-        <?
-        $users = get_field("post_meta_team", $id);
-        if ($users) :
-        ?>
-            <div class="post-card__meta__team">
-                <div class="post-card__meta__team__images__wrapper">
-                    <img src="<?= esc_attr(get_avatar_url('danilaprok20@gmail.com')); ?>" alt="Аватар пользователя" class="post-card__meta__teaam__imge__source">
-                    <? foreach ($users as $user) : ?>
-                        <img src="<?= esc_attr(get_avatar_url($user['ID'])); ?>" alt="Аватар пользователя" class="post-card__meta__teaam__imge__source">
-                    <? endforeach; ?>
-                </div>
-                <div class="post-card__meta__team__name">
-                    Совместно с
-                    <? foreach ($users as $user) : ?>
-                        <a href="<?= esc_attr($user['user_url']) ?>">
-                            <?= esc_attr($user['nickname']) ?>
-                        </a>
-                    <? endforeach; ?>
-                </div>
-            </div>
-        <? endif; ?>
-    </div>
-
     <div class="post-card__big__gallery">
         <div class="post-card__big__gallery__background f-carousel" data-gallery="<?= $id ?>" id="1">
             <div class="f-carousel__slide">
@@ -119,4 +61,55 @@ if ($args) {
             ?>
         </div>
     </div>
+    <div class="post-card__big__content__wrapper">
+        <div class="post-card__big__content">
+            <div class="post-card__big__title">
+                <div class="post-card__big__heading">
+                    <?= $title ?>
+                </div>
+                <div class="post-card__big__short-description">
+                    <?= $short_description ?>
+                </div>
+            </div>
+            <?
+            $post_meta_repeater = get_field('post_meta-repeater', $id);
+            if (have_rows('post_meta-repeater', $id)) :
+            ?>
+                <div class="post-card__big__meta">
+                    <?
+                    while (have_rows('post_meta-repeater', $id)) : the_row();
+                        $post_meta_variable = get_sub_field('post_meta-variable');
+                        $post_meta_value = get_sub_field('post_meta-value');
+                    ?>
+                        <div class="post-card__big__meta__item">
+                            <div class="post-card__big__meta__item__variable"><?= $post_meta_variable ?></div>
+                            <div class="post-card__big__meta__item__value"><?= $post_meta_value ?></div>
+                        </div>
+                    <? endwhile; ?>
+                </div>
+            <? endif ?>
+        </div>
+        <?
+        $users = get_field("post_meta_team", $id);
+        if ($users) :
+        ?>
+            <div class="post-card__meta__team">
+                <div class="post-card__meta__team__images__wrapper">
+                    <img src="<?= esc_attr(get_avatar_url('danilaprok20@gmail.com')); ?>" alt="Аватар пользователя" class="post-card__meta__teaam__imge__source">
+                    <? foreach ($users as $user) : ?>
+                        <img src="<?= esc_attr(get_avatar_url($user['ID'])); ?>" alt="Аватар пользователя" class="post-card__meta__teaam__imge__source">
+                    <? endforeach; ?>
+                </div>
+                <div class="post-card__meta__team__name">
+                    Совместно с
+                    <? foreach ($users as $user) : ?>
+                        <a href="<?= esc_attr($user['user_url']) ?>">
+                            <?= esc_attr($user['nickname']) ?>
+                        </a>
+                    <? endforeach; ?>
+                </div>
+            </div>
+        <? endif; ?>
+    </div>
+
 </div>
