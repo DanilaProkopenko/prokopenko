cc72582fde661f17b895cf0ea5ddc153de6caf57
DanilaProkopenko <danilaprok20@gmail.com>
2024-10-30 22:05:52 +0300

    many changes

--- functions.php
+++ functions.php
@@ -24,5 +24,30 @@ function wwzrds_init_global_acf_constants()
 	define('GLOBAL_EXAMPLE_FIELD_VALUE', get_field('global_example_field', 'options'));
 }
 
+/**
+ * Removes or edits the 'Protected:' part from posts titles
+ */
+
+add_filter('protected_title_format', 'remove_protected_text');
+function remove_protected_text()
+{
+	return __('NDA: %s');
+}
+
+function my_password_form()
+{
+	global $post;
+	$label = 'pwbox-' . (empty($post->ID) ? rand() : $post->ID);
+	$o = '<form action="' . esc_url(site_url('wp-login.php?action=postpass', 'login_post')) . '" method="post" class="post-password-form" data-bitwarden-watching="1">
+    ' . __("Этот контент защищен паролем. Для просмотра введите свой пароль ниже:") . '
+    <label for="' . $label . '">' . __("Пароль:") . ' </label>
+	<input name="post_password" id="' . $label . '" type="password" size="20" maxlength="20" />
+	<input type="submit" name="Submit" value="' . esc_attr__("Submit") . '" />
+    </form>
+    ';
+	return $o;
+}
+add_filter('the_password_form', 'my_password_form');
+
 require_once THEME_DIR . '/includes/load.php';
 require_once THEME_DIR . '/blocks/load.php';
