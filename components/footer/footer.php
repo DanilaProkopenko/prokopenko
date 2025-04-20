cc96055192dabe60d25aa73a3ad6855998313b4d
Danila Prokopenko <danilaprok20@gmail.com>
2025-04-20 22:32:37 +0300

    fix

--- components/footer/footer.php
+++ components/footer/footer.php
@@ -4,30 +4,6 @@ $footer_date = get_field('footer-date', 'Options');
 ?>
 <footer class="footer">
     <div class="footer__content">
-        <!-- <div class="footer__links__wrapper">
-            <?php
-            $args = array(
-                'menu' => 'footer_first',
-                'depth'    => 0,
-                'container' => 'div',
-                'menu_class' => 'footer__links list-style-none',
-                'fallback_cb' => false
-            );
-
-            wp_nav_menu($args);
-            ?>
-            <?php
-            $args = array(
-                'menu' => 'footer_second',
-                'depth'    => 0,
-                'container' => 'div',
-                'menu_class' => 'footer__links list-style-none',
-                'fallback_cb' => false
-            );
-
-            wp_nav_menu($args);
-            ?>
-        </div> -->
         <?php if ($footer_disclaimer) : ?>
             <div class="footer__disclaimer">
                 <?= $footer_disclaimer ?>
