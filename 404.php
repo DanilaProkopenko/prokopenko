69293acd35704a097a6184658ae04fdc74be28ea
Danila Prokopenko <danilaprok20@gmail.com>
2025-06-26 13:02:30 +0300

    добавил 404 страницу

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+<div class="wp-block-columns _padding section is-layout-flex wp-container-core-columns-is-layout-28f84493 wp-block-columns-is-layout-flex">
+    <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow"></div>
+    <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
+        <h1 class="wp-block-heading">
+            Ошибка 404
+        </h1>
+        <p class="title__caption">Страница не&nbsp;существует. Возможно, ошибка в&nbsp;адресе, или она была удалена. Можете посмотреть <a href="/#favorites" data-type="internal" data-id="#favorites">избранные работы</a> или&nbsp;<a href="/works-feed">архив.</a></p>
+    </div>
+</div>
+
+<?php
+get_footer();
